10dcf006b02f3616d3f0a3d0c8fd71657e057aff
jdiorio2393 <jdiorio2393@gmail.com>
2019-06-23 00:07:38 -0400

    JAWS DB Edits

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+  
+    password: "root",
+    database: "burgers_db"
+  });
+  
+}
 
-  password: "root",
-  database: "burgers_db"
-});
 
 connection.connect(function(err) {
     if (err) {
